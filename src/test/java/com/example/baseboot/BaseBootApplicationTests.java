f1690f9700cc74fda24fd37c306015f204dbab55
ikkuy <cjt199105@163.com>
2024-01-15 21:55:47 +0800

    first commit

---
+++ src/test/java/com/example/baseboot/BaseBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baseboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
